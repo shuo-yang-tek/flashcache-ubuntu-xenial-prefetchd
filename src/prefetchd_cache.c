9209229e70c5db8c5f57560fd9bfcdb886c46e21
Shuo Yang <shuo.yang.tek@gmail.com>
2018-10-19 03:41:46 +0800

    fix

--- src/prefetchd_cache.c
+++ src/prefetchd_cache.c
@@ -261,7 +261,7 @@ bool prefetchd_cache_handle_bio(struct bio *bio) {
 
 	bio_endio(bio);
 
-	DPPRINTK("----hh");
+	DPPRINTK("----ii");
 
 	cache_meta_map_foreach(map, meta, i) {
 		atomic_dec(&(meta->hold_count));
