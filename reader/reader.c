1a7f9676f63b966bfe55a61c62290dbef416c81c
Shuo Yang <shuo.yang.tek@gmail.com>
2018-10-19 23:43:17 +0800

    fix

---
+++ reader/reader.c
@@ -0,0 +1,74 @@
+#define SIZE_PER_READ 4096
+#define STRIDE_LEN 4096
+#define COUNT 100000
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+	int type = 0;
+	char *dev, *str;
+	int fd;
+	char buf[SIZE_PER_READ];
+	int i;
+	int seek_type = SEEK_SET;
+	off_t seek_offset = 0;
+	struct timespec start;
+	struct timespec end;
+	double spent;
+
+	if (argc < 2) {
+		printf("reader <path> [type]\n");
+		return -1;
+	}
+
+	dev = argv[1];
+
+	fd = open(dev, O_RDONLY);
+	if (fd < 0) {
+		printf("can't open %s\n", dev);
+		return -1;
+	}
+	
+	if (argc > 2) {
+		str = argv[2];
+		if (strcmp(str, "seq-back") == 0) {
+			type = 1;
+			seek_type = SEEK_END;
+		} else if (strcmp(str, "str-for") == 0) type = 2;
+		else if (strcmp(str, "str-back") == 0) {
+			type = 3;
+			seek_type = SEEK_END;
+		}
+	}
+
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	for (i = 0; i < COUNT; i++) {
+		if (lseek(fd, seek_offset, seek_type) < 0) {
+			printf("seek fail\n");
+			break;
+		}
+		if (read(fd, buf, SIZE_PER_READ) < 0) {
+			printf("read fail\n");
+			break;
+		}
+		seek_offset += SIZE_PER_READ;
+		if (type >= 2) seek_offset += STRIDE_LEN;
+	}
+
+	clock_gettime(CLOCK_REALTIME, &end);
+
+	spent = (double)(end.tv_sec - start.tv_sec) + ((double)end.tv_nsec - (double)end.tv_nsec) / 1.0e9;
+	printf("%lfs\n", spent);
+
+	close(fd);
+	return 0;
+}
