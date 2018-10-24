55a1f32c7dfa96272159b88cc5db8e45fafdd989
Shuo Yang <shuo.yang.tek@gmail.com>
2018-10-24 23:14:31 +0800

    fix

--- src/pfd_cache.h
+++ src/pfd_cache.h
@@ -1,6 +1,8 @@
 #define PFD_CACHE_COUNT_PER_SET 4
 #define PFD_CACHE_BLOCK_COUNT 16384
 
+#include <stdbool.h>
+
 void pfd_cache_init(void);
 void pfd_cache_exit(void);
 void pfd_cache_add(struct cache_c *dmc);
