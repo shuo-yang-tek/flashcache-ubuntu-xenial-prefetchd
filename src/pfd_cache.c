d39252301881c8aa6b71b4fabdf08e31027f9ab1
Shuo Yang <shuo.yang.tek@gmail.com>
2018-10-25 16:32:17 +0800

    fix

--- src/pfd_cache.c
+++ src/pfd_cache.c
@@ -715,9 +715,9 @@ update_dispatch_req_pool(
 
 	if (dbn + (long)dmc->block_size == *start) {
 		*start = dbn;
-		*count += 1;
+		(*count) += 1;
 	} else if (*start + ((long)(*count) << dmc->block_shift) == dbn)
-		*count += 1;
+		(*count) += 1;
 	else {
 		flush_dispatch_req_pool(cache, *start, *count);
 		*start = dbn;
